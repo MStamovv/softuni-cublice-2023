39af62bc458bd1ef4883767e2edd67c9a0ec7bdb
MStamovv <114014772+MStamovv@users.noreply.github.com>
2023-06-03 16:29:03 +0300

    Add id generator logic

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,13 +1,16 @@
+const uniqid = require(`uniqid`)
 const cubes = [];
 
-exports.getAll = ()=> cubes.slice();
+exports.getAll = () => cubes.slice();
+
+
 
 exports.create = (cubeData) => {
     const newCube = {
-        id: cubes.length + 1,
+        id: uniqid(),
         ...cubeData,
     };
-    
+
     cubes.push(newCube);
     return newCube;
 
