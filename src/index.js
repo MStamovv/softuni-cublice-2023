6df196d31dcee3204bd0259bb05d9dd4920e3d5c
MStamovv <114014772+MStamovv@users.noreply.github.com>
2023-06-03 13:45:46 +0300

    Add handlebars to project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log(`Hello world`);
+const express = require(`express`);
+const handlebars = require(`express-handlebars`);
+
+const app = express();
+
+const port = 5000;
+
+
+// Handlebars confing
+app.engine(`hbs`, handlebars.engine({
+    extname: "hbs",
+}));
+app.set(`view engine`, `hbs`);
+app.set(`views`, `src/views`);
+
+// Routes
+app.get(`/`, (req, res) => {
+    res.render(`index`);
+});
+
+
+app.listen(port, () => console.log(`Server is running on ${port}...`));
+
