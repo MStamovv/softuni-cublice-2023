238ebd6f67fbb3a6b51a487adc02c6e760a9c23a
MStamovv <114014772+MStamovv@users.noreply.github.com>
2023-06-08 19:22:00 +0300

    Setup mongoose

--- src/index.js
+++ src/index.js
@@ -3,22 +3,19 @@ const express = require(`express`);
 
 const expressConfig = require(`./config/expressConfig`);
 const handlebarsConfig = require(`./config/handlebarsConfig`);
-const homeController = require(`./controllers/homeController`);
-const cubeController = require(`./controllers/cubeController`);
+const routes = require(`./routes`);
+
 
 const app = express();
 
 const port = 5000;
 
+
+
 expressConfig(app);
 handlebarsConfig(app);
 
-app.use(homeController);
-app.use(`/cubes`,cubeController);
-app.get(`*`, (req , res) =>{
-    res.redirect(`/404`);
-});
-
+app.use(routes);
 
 
 app.listen(port, () => console.log(`Server is running on ${port}...`));
