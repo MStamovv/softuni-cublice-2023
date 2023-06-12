7cc3849ed74811ffb35a43f3b7f29738d37bfae8
MStamovv <114014772+MStamovv@users.noreply.github.com>
2023-06-12 13:13:49 +0300

    Add base cube model

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const express = require(`express`);
 const expressConfig = require(`./config/expressConfig`);
 const handlebarsConfig = require(`./config/handlebarsConfig`);
 const routes = require(`./routes`);
+const dbConnect = require(`./config/dbConfig`);
+
+
 
 
 const app = express();
@@ -12,11 +15,22 @@ const port = 5000;
 
 
 
+
+
 expressConfig(app);
 handlebarsConfig(app);
+
+dbConnect()
+.then(() => console.log(`DB connect successfully`))
+.catch(err => {console.log(`DB error : `, err)});
+
+
 
 app.use(routes);
 
 
 app.listen(port, () => console.log(`Server is running on ${port}...`));
+
+
+
 
